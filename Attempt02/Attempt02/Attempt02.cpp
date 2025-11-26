315beb81f7bad506f8644e9360784d8ea52e730a
15634fernando <15634@ait.nsw.edu.au>
2025-11-26 17:10:24 +1100

    Task 3 - Separated Ebooks to Hard Copy

--- Attempt02/Attempt02/Attempt02.cpp
+++ Attempt02/Attempt02/Attempt02.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 class Book {
-private:
+protected:
     string title;
     string author;
     string ISBN;
@@ -20,7 +20,7 @@ public:
         title(t), author(a), ISBN(i), available(av), dateAdded(date) {
     }
 
-    void displayDetails() {
+    virtual void displayDetails() const {
         cout << "Title: " << title << endl;
         cout << "Author: " << author << endl;
         cout << "ISBN: " << ISBN << endl;
@@ -28,64 +28,74 @@ public:
         cout << "Date Added: " << dateAdded << endl;
     }
 
-    bool isAvailable() const {
-        return available;
-    }
-
     void borrowBook() {
         if (available) {
             available = false;
-            cout << "You have borrowed \"" << title << "\"." << endl;
+            cout << "You borrowed \"" << title << "\".\n";
         }
         else {
-            cout << "Sorry, \"" << title << "\" is not available right now." << endl;
+            cout << "\"" << title << "\" is not available.\n";
         }
-    }
-
-    const string& getISBN() const {
-        return ISBN;
     }
 };
 
-int findBookByISBN(const vector<Book>& books, const string& isbn) {
-    for (size_t i = 0; i < books.size(); ++i) {
-        if (books[i].getISBN() == isbn) {
-            return i;
-        }
+class HardcopyBook : public Book {
+private:
+    string shelfNumber;
+
+public:
+    HardcopyBook(string t, string a, string i, bool av, string date, string shelf) :
+        Book(t, a, i, av, date), shelfNumber(shelf) {
     }
-    return -1;
-}
+
+    void displayDetails() const override {
+        Book::displayDetails();
+        cout << "Shelf Number: " << shelfNumber << endl;
+    }
+};
+
+class EBook : public Book {
+private:
+    string licenseEndDate;
+
+public:
+    EBook(string t, string a, string i, bool av, string date, string licenseDate) :
+        Book(t, a, i, av, date), licenseEndDate(licenseDate) {
+    }
+
+    void displayDetails() const override {
+        Book::displayDetails();
+        cout << "License End Date: " << licenseEndDate << endl;
+    }
+};
 
 int main() {
-    vector<Book> books = {
-        Book("Chainsaw Man Vol. 01", "Tatsuki Fujimoto", "9781974709939", true, "2025-11-01"),
-        Book("The Maze Runner", "James Dashner", "9780385737951", true, "2025-11-01"),
-        Book("Percy Jackson and the Sea of Monsters", "Rick Riordan", "9780141381497", true, "2025-11-01"),
-        Book("Attack on Titan Vol. 01", "Hajime Isayama", "9781612626154", true, "2025-11-01"),
-        Book("Jujutsu Kaisen Vol. 01", "Gege Akutami", "9781974720149", true, "2025-11-01")
+    vector<HardcopyBook> hardbooks = {
+        HardcopyBook("Chainsaw Man Vol. 01", "Tatsuki Fujimoto", "9781974709939", true, "2025-11-01", "A12"),
+        HardcopyBook("The Maze Runner", "James Dashner", "9780385737951", true, "2025-11-01", "B7")
     };
 
-    string isbnInput;
-    cout << "Welcome to the Book Management System\n";
+    vector<EBook> ebooks = {
+        EBook("Percy Jackson and the Sea of Monsters", "Rick Riordan", "9780141381497", true, "2025-11-01", "2026-12-31"),
+        EBook("Attack on Titan Vol. 01", "Hajime Isayama", "9781612626154", true, "2025-11-01", "2025-11-30"),
+        EBook("Jujutsu Kaisen Vol. 01", "Gege Akutami", "9781974720149", true, "2025-11-01", "2025-12-31")
+    };
 
-    while (true) {
-        cout << "\nEnter ISBN of book to borrow (0 to exit): ";
-        getline(cin, isbnInput);
-
-        if (isbnInput == "0") {
-            cout << "Exiting the system. Goodbye!\n";
-            break;
-        }
-
-        int index = findBookByISBN(books, isbnInput);
-
-        if (index == -1) {
-            cout << "Book with ISBN " << isbnInput << " not found.\n";
-        }
-        else {
-            books[index].borrowBook();
-        }
+    cout << "Hardcopy Books:\n";
+    for (const auto& book : hardbooks) {
+        book.displayDetails();
+        cout << endl;
     }
+
+    cout << "E-Books:\n";
+    for (const auto& book : ebooks) {
+        book.displayDetails();
+        cout << endl;
+    }
+
+    // Example borrowing books
+    hardbooks[0].borrowBook();
+    ebooks[0].borrowBook();
 
     return 0;
 }
